c045818af52682cbc3c44c081d9889837ef6abd2
Thiago Guimaraes <thiagogsr@gmail.com>
2014-10-03 20:01:01 -0300

    Added exception to get value in any side

--- basic_tree/tree.rb
+++ basic_tree/tree.rb
@@ -4,18 +4,21 @@ class Tree
     @root = nil
   end
 
-  def insert value, branch, side
-    @root = branch if @root.nil?
-
+  def insert value, branch = nil, side = nil
     new_branch = Branch.new value
-    new_branch.parent = branch
 
-    branch.children[side] = new_branch
+    if @root.nil?
+      @root = new_branch
+    else
+      new_branch.parent = branch
+      branch.children[side] = new_branch
+    end
+
     new_branch
   end
 
-  def next branch, side
-    branch.children[side]
+  def get branch, side
+    branch.children[side].value rescue 'nenhum valor neste lado'
   end
 
 end
